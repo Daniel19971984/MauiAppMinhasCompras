ab4f3cf9f3783a26464e663252f03fda36aa8df3
Daniel de Souza <danieltecnoads@gmail.com>
2025-09-07 17:22:25 -0300

    Finalização do basico do app remover e atualizar

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,31 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+
+        static SQLiteDatabaseHelper _DB;
+
+        public static SQLiteDatabaseHelper DB 
+        { 
+             get 
+
+             {
+                  if(_DB ==null)
+                  {
+                    string path = Path.Combine(Environment.GetFolderPath
+                        (Environment.SpecialFolder.LocalApplicationData),
+                 "banco_sqlite_compras.db3");
+                  
+                    _DB = new SQLiteDatabaseHelper(path);
+                  }
+
+                return _DB; 
+            
+             } 
+        
+        }
         public App()
         {
             InitializeComponent();
--- Helpers/SQLiteDatabaseHelper.cs
+++ Helpers/SQLiteDatabaseHelper.cs
@@ -5,40 +5,43 @@ namespace MauiAppMinhasCompras.Helpers
 {
     public class SQLiteDatabaseHelper
     {
-        readonly SQLiteAsyncConnection _connection;
+        readonly SQLiteAsyncConnection _conn;
+
         public SQLiteDatabaseHelper(string path)
         {
-            _connection = new SQLiteAsyncConnection(path);
-            _connection.CreateTableAsync<Produto>().Wait();
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
         }
 
-        public Task<int> insert(Produto p)
+        public Task<int> Insert(Produto p)
         {
-            return _connection.InsertAsync(p);
+            return _conn.InsertAsync(p);
         }
 
         public Task<List<Produto>> Update(Produto p)
         {
-            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=?, WHERE ID=?";
-           return _connection.QueryAsync<Produto>(sql, p.Descricao, p.Preco, p.Id);
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+            );
         }
 
-
-        public Task<int> delete(int id) 
+        public Task<int> Delete(int id)
         {
-         return _connection.Table<Produto>().DeleteAsync(i  => i.Id == id);
-
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
         }
-        public Task<List<Produto>> GetAll() 
+
+        public Task<List<Produto>> GetAll()
         {
-            return _connection.Table<Produto>().ToListAsync();
+            return _conn.Table<Produto>().ToListAsync();
         }
 
-        public Task<List<Produto>> Search (string query) 
+        public Task<List<Produto>> Search(string q)
         {
-            string sql = "SELECT * Produto where descricao LIKE '%" + q + "%'";
-            return _connection.QueryAsync<Produto> (sql);
-        }
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%'";
 
+            return _conn.QueryAsync<Produto>(sql);
+        }
     }
 }
